2f3c8d11391d5255d851129e38e963a76eeb0756
Paul Hanchett <paul.hanchett@gmail.com>
2017-06-18 14:27:04 -0700

    Fixed (set) install of required packages. Set url correctly for github repository.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+install_requires = {
+      'os',
+      'logging',
+      'yaml'
+}
+
 setup(name='logger_yaml',
       version='0.1',
       description='Configure logger using YAML file (example included)',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/paulha/logger_yaml.git',
       author='Paul Hanchett',
       author_email='paul.hanchett@gmail.com',
       license='MIT',
