e5e3ea6c18ae378e3511d47368bbf13d03d533ae
Paul Hanchett <paul.hanchett@gmail.com>
2017-06-17 18:58:42 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='logger_yaml',
+      version='0.1',
+      description='Configure logger using YAML file (example included)',
+      url='http://github.com/storborg/funniest',
+      author='Paul Hanchett',
+      author_email='paul.hanchett@gmail.com',
+      license='MIT',
+      packages=['logger_yaml'],
+      zip_safe=False)
